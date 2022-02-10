1c65ae3f12067c286ba25d5cffff46b32c3224e3
Maik-Karimi <95415317+Maik-Karimi@users.noreply.github.com>
2022-02-10 11:57:33 +0000

    user array

--- user_system.c
+++ user_system.c
@@ -7,3 +7,65 @@
 // AVAILABLE HERE: Function print_user(id, name, email) to print a single user
 // Make sure to write empty functions for ALL functions in the header (also optional ones),
 // otherwise the program will not compile
+
+typedef struct users_tag  {
+    char *name;
+    char *email;
+    user_id user_id;
+}users;
+int size = 0;
+
+users *userlist;
+void users_initialize_table(int x) {
+    size = x;
+ userlist = (users*) malloc(sizeof(users) * size);
+ for(int i = 0; i<size ;i++) {
+   userlist[i].name = NULL;  
+ }
+}
+
+
+   
+
+
+
+user_id users_add(char *name, char*email){
+    int i = 0;
+   while(userlist[i].name != NULL) {
+    if(i >=size){
+        return -1;
+    }
+    i++;
+   } 
+userlist[i].name = name;
+userlist[i].email = email;
+userlist[i].user_id = i;
+return userlist[i]. user_id;
+}
+
+void users_printall(){
+    for (int i = 0; userlist[i].name != NULL ; i++)
+    {
+        if(i < size) {
+        print_user(userlist[i].user_id,userlist[i].name,userlist[i].email);
+
+        }else {
+            break;
+        }
+     
+    }
+    
+}
+char *users_get_name(user_id id){
+   return userlist[id].name; 
+}
+
+void users_delete(user_id id){
+
+}
+
+void users_resize(int new_capacity) {
+
+}
+
+
